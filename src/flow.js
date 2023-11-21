8fb97c33465ad4ee6938fb7a26cf1bc5a72b6522
Wolf <wolfverelst@gmail.com>
2023-11-21 13:01:19 +0100

    fix: eventlistener error

--- src/flow.js
+++ src/flow.js
@@ -1,1 +1,28 @@
-let h1=document.querySelector("#title"),addLol=function(){alert("hallo")};h1.addEventListener('click');const galleryItems=document.querySelectorAll('.gallery-item'),overlay=document.querySelector('.overlay'),overlayImage=document.querySelector('#overlay-image'),closeButton=document.querySelector('#close-button');galleryItems.forEach(item=>{item.addEventListener('click',()=>{const anchor=item.querySelector('a'),pageUrl=anchor.getAttribute('href');window.location.href=pageUrl})}),closeButton.addEventListener('click',()=>{overlay.style.display='none'});const imageContainer=document.querySelector('.image-container'),scrollImages=function(){const firstImage=imageContainer.firstElementChild;imageContainer.appendChild(firstImage)};setInterval(scrollImages,2000);
+const galleryItems = document.querySelectorAll('.gallery-item'),
+    overlay = document.querySelector('.overlay'),
+    overlayImage = document.querySelector('#overlay-image'),
+    closeButton = document.querySelector('#close-button');
+
+galleryItems.forEach(item => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the default behavior of the anchor element
+        const anchor = item.querySelector('a');
+        const pageUrl = anchor.getAttribute('href');
+        window.location.href = pageUrl;
+    });
+});
+
+// Check if closeButton and overlay are not null before adding the event listener
+if (closeButton && overlay) {
+    closeButton.addEventListener('click', () => {
+        overlay.style.display = 'none';
+    });
+}
+
+const imageContainer = document.querySelector('.image-container'),
+    scrollImages = function () {
+        const firstImage = imageContainer.firstElementChild;
+        imageContainer.appendChild(firstImage);
+    };
+
+setInterval(scrollImages, 2000);

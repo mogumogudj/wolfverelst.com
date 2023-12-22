8cb8c683958f60ec5e5541bcb09c412d10c9f6cd
Wolf <wolfverelst@gmail.com>
2023-12-22 15:55:57 +0100

    fix: email copy redirecting to new page

--- src/flow.js
+++ src/flow.js
@@ -27,3 +27,27 @@ const imageContainer = document.querySelector('.image-container'),
 
 setInterval(scrollImages, 20000);
 console.log(scrollImages);
+
+document.addEventListener('DOMContentLoaded', function () {
+    const emailIcon = document.getElementById('sidebar-left-email-icon');
+    const popup = document.getElementById('copyPopup');
+    const emailAddress = 'wolfverelst@gmail.com'; // Replace with your actual email address
+
+    emailIcon.addEventListener('click', function () {
+        const tempInput = document.createElement('input');
+        tempInput.value = emailAddress;
+        document.body.appendChild(tempInput);
+
+        tempInput.select();
+        document.execCommand('copy');
+        document.body.removeChild(tempInput);
+
+        // Show the styled popup
+        popup.style.display = 'block';
+
+        // Hide the popup after 2 seconds (adjust as needed)
+        setTimeout(function () {
+            popup.style.display = 'none';
+        }, 2000);
+    });
+});

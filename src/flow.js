dbc7fca31143c322ee9719b6dc7322cc45ba190d
Wolf <wolfverelst@gmail.com>
2024-04-04 16:32:38 +0200

    fix: Toggle Sidebar issue

--- src/flow.js
+++ src/flow.js
@@ -1,3 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('sidebar.html')
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('sidebarContainer').innerHTML = html;
+
+            const toggleButton = document.getElementById('toggleSidebar');
+            const sidebar = document.querySelector('#sidebarContainer .sidebar-right'); // Target the sidebar within #sidebarContainer
+
+            if (toggleButton && sidebar) {
+                toggleButton.addEventListener('click', function() {
+                    sidebar.classList.toggle('show-sidebar');
+                });
+            } else {
+                console.error('Toggle button or sidebar element not found.');
+            }
+        });
+
+    const scrollImages = function() {
+        const imageContainer = document.querySelector('.image-container');
+        const firstImage = imageContainer.firstElementChild;
+        imageContainer.appendChild(firstImage);
+    };
+
+    setInterval(scrollImages, 38000);
+});
+
+
 
 
 
